723744c96a40901f29266f914835a45abe9510ce
Vventeo <manuelventeo09@gmail.com>
2024-11-02 20:35:36 -0300

    carpetas express y react agragadas pero no termine de instalar todo

---
+++ express-api/app.js
@@ -0,0 +1,15 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import authRoutes from "./routes/auth.routes.js";
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('esa que pondavmiasyiogbsaubc!')
+})
+
+app.listen(port, () => {
+  console.log(`el server esta correindo en: ${port}`)
+})
